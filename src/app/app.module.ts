feba0c59bbf5344e1afa70e8e92ae8a695fe35de
Alejandro Bravo <alejandro.bravoccc@gmail.com>
2024-05-08 12:27:21 -0400

    componentes creados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,18 @@ import { AppComponent } from './app.component';
 import { CounterModule } from './counter/counter.module';
 
 import { HeroesModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     CounterModule,
-    HeroesModule
+    HeroesModule,
+    DbzModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
